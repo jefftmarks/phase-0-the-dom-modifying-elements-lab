4497c536eed7301a7efb7865560b776f86ebea48
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-19 20:11:26 -0400

    commit tree 44c3550f9f9a5f9fef9e3c26e8139206c5fb9a74

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-document.querySelector("main#main").remove();
+// document.querySelector("main#main").remove();
 
-const newHeader = document.createElement("h1");
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory";
+// newHeader.textContent = "YOUR-NAME is the champion";
+// document.body.append(newHeader);
+
+document.querySelector("#main").remove();
+
+const newHeader = document.createElement('h1');
 newHeader.id = "victory";
 newHeader.textContent = "YOUR-NAME is the champion";
-document.body.append(newHeader);
